e799afec6901d9dba2bb9bdfe4a776a893fcc337
amasini0 <masini.alessandro94@gmail.com>
2026-01-18 22:20:21 +0100

    Fix build.rs script for 02_UniformBuffer

--- 02_UniformBuffer/build.rs
+++ 02_UniformBuffer/build.rs
@@ -5,7 +5,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
     // Slang shader
-    println!("cargo::rerun-if-changed=shaders/hello.slang");
+    println!("cargo::rerun-if-changed=shaders/print.slang");
     Command::new("slangc")
         .args(&["shaders/print.slang", "-target", "spirv", "-o"])
         .arg(&format!("{}/print.spv", out_dir))
